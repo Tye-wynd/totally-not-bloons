ed7fe59c0168dbe3bb494d55ce28b973bce55dba
Tye-wynd <167675082+Tye-wynd@users.noreply.github.com>
2026-05-01 14:42:24 -0400

    uploaded a test gdevelop html to make sure ts works

---
+++ code0.js
@@ -0,0 +1,43 @@
+gdjs.Untitled_32sceneCode = {};
+gdjs.Untitled_32sceneCode.localVariables = [];
+gdjs.Untitled_32sceneCode.idToCallbackMap = new Map();
+gdjs.Untitled_32sceneCode.GDNewSpriteObjects1= [];
+gdjs.Untitled_32sceneCode.GDNewSpriteObjects2= [];
+gdjs.Untitled_32sceneCode.GDNewVideoObjects1= [];
+gdjs.Untitled_32sceneCode.GDNewVideoObjects2= [];
+
+
+gdjs.Untitled_32sceneCode.eventsList0 = function(runtimeScene) {
+
+{
+
+
+let isConditionTrue_0 = false;
+{
+}
+
+}
+
+
+};
+
+gdjs.Untitled_32sceneCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+gdjs.Untitled_32sceneCode.GDNewSpriteObjects1.length = 0;
+gdjs.Untitled_32sceneCode.GDNewSpriteObjects2.length = 0;
+gdjs.Untitled_32sceneCode.GDNewVideoObjects1.length = 0;
+gdjs.Untitled_32sceneCode.GDNewVideoObjects2.length = 0;
+
+gdjs.Untitled_32sceneCode.eventsList0(runtimeScene);
+gdjs.Untitled_32sceneCode.GDNewSpriteObjects1.length = 0;
+gdjs.Untitled_32sceneCode.GDNewSpriteObjects2.length = 0;
+gdjs.Untitled_32sceneCode.GDNewVideoObjects1.length = 0;
+gdjs.Untitled_32sceneCode.GDNewVideoObjects2.length = 0;
+
+
+return;
+
+}
+
+gdjs['Untitled_32sceneCode'] = gdjs.Untitled_32sceneCode;
