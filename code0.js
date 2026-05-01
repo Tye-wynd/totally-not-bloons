734883d910cbc307c2f8929d8b16456b67b98c31
Tye-wynd <167675082+Tye-wynd@users.noreply.github.com>
2026-05-01 15:11:26 -0400

    Second test
    this time with moving parts

--- code0.js
+++ code0.js
@@ -11,9 +11,24 @@ gdjs.Untitled_32sceneCode.eventsList0 = function(runtimeScene) {
 
 {
 
+gdjs.copyArray(runtimeScene.getObjects("NewVideo"), gdjs.Untitled_32sceneCode.GDNewVideoObjects1);
 
 let isConditionTrue_0 = false;
-{
+isConditionTrue_0 = false;
+for (var i = 0, k = 0, l = gdjs.Untitled_32sceneCode.GDNewVideoObjects1.length;i<l;++i) {
+    if ( gdjs.Untitled_32sceneCode.GDNewVideoObjects1[i].behaviorActivated("ButtonFSM") ) {
+        isConditionTrue_0 = true;
+        gdjs.Untitled_32sceneCode.GDNewVideoObjects1[k] = gdjs.Untitled_32sceneCode.GDNewVideoObjects1[i];
+        ++k;
+    }
+}
+gdjs.Untitled_32sceneCode.GDNewVideoObjects1.length = k;
+if (isConditionTrue_0) {
+/* Reuse gdjs.Untitled_32sceneCode.GDNewVideoObjects1 */
+{for(var i = 0, len = gdjs.Untitled_32sceneCode.GDNewVideoObjects1.length ;i < len;++i) {
+    gdjs.Untitled_32sceneCode.GDNewVideoObjects1[i].play();
+}
+}
 }
 
 }
